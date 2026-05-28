0db861462a0d9def51abb2fd1eb3d8deae5b5a83
Ray <zaphim12@gmail.com>
2026-05-27 23:58:00 -0400

    Automatically reveal all trait, relic, and spell description texts from run-start

---
+++ Patches/ItemDescriptionPatch.cs
@@ -0,0 +1,35 @@
+using HarmonyLib;
+
+namespace RL2Archipelago.Patches;
+
+[HarmonyPatch]
+internal static class ItemDescriptionPatch
+{
+    // Spells: shown as "???" on heir screen when GetSpellSeenState returns false
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PlayerSaveData), nameof(PlayerSaveData.GetSpellSeenState))]
+    private static void SpellSeenState_Postfix(ref bool __result)
+    {
+        if (!APClient.IsConnected) return;
+        __result = true;
+    }
+
+    // Traits: shown as "???" on heir screen when GetTraitSeenState < SeenOnce
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(TraitManager), nameof(TraitManager.GetTraitSeenState))]
+    private static void TraitSeenState_Postfix(ref TraitSeenState __result)
+    {
+        if (!APClient.IsConnected) return;
+        if (__result < TraitSeenState.SeenOnce)
+            __result = TraitSeenState.SeenOnce;
+    }
+
+    // Relics: shown as "???" on heir screen and in-run pedestal when WasSeen is false
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(RelicObj), nameof(RelicObj.WasSeen), MethodType.Getter)]
+    private static void RelicWasSeen_Postfix(ref bool __result)
+    {
+        if (!APClient.IsConnected) return;
+        __result = true;
+    }
+}
